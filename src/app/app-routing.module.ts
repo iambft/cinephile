b59106e4930f5cbfc63807a3fd6d8c8697c6bf13
Kyrylo Kostichev <Kyrylo.Kostichev@Sophos.com>
2019-12-09 16:45:57 +0200

    create backbone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {LikedComponent} from './pages/liked/liked.component';
+import {TopListComponent} from './pages/top-list/top-list.component';
+import {MainComponent} from './pages/main/main.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'top-list', component: TopListComponent },
+  { path: 'liked', component: LikedComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { MoveCardComponent } from './shared/components/move-card/move-card.component';
+import { MainComponent } from './pages/main/main.component';
+import { TopListComponent } from './pages/top-list/top-list.component';
+import { LikedComponent } from './pages/liked/liked.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoveCardComponent,
+    MainComponent,
+    TopListComponent,
+    LikedComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
